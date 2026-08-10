e570be4e8801d828016fb18894b614e3d8aeb089
Thales <devthalespaloski@gmail.com>
2026-08-10 14:50:25 -0300

    feat: implementa validação de dados para produto e venda

--- src/routes/produto.route.js
+++ src/routes/produto.route.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import ProdutoController from "../controllers/produto.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import validator from "../middlewares/validator.middleware.js";
+import { validadorAtualizarProduto, validadorCadastroProduto } from "../validators/produto.validator.js";
 
 const produtoRouter = Router();
 const produtoController = new ProdutoController();
 
 produtoRouter.get('/produtos', produtoController.listar);
 produtoRouter.get('/produtos/:id', produtoController.listarPorId);
-produtoRouter.post('/produtos', authMiddleware, produtoController.cadastrar);
-produtoRouter.put('/produtos/:id', authMiddleware, produtoController.atualizar);
+produtoRouter.post('/produtos', authMiddleware, validator(validadorCadastroProduto), produtoController.cadastrar);
+produtoRouter.put('/produtos/:id', authMiddleware, validator(validadorAtualizarProduto), produtoController.atualizar);
 produtoRouter.delete('/produtos/:id', authMiddleware, produtoController.deletar);
 
 export default produtoRouter;
--- src/routes/venda.route.js
+++ src/routes/venda.route.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import VendaController from "../controllers/venda.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import validator from "../middlewares/validator.middleware.js";
+import { validadorCadastroVenda } from "../validators/venda.validator.js";
 
 const vendaRouter = Router();
 const vendaController = new VendaController();
 
 vendaRouter.get('/vendas', authMiddleware, vendaController.listar);
 vendaRouter.get('/vendas/:id', authMiddleware, vendaController.listarPorId);
-vendaRouter.post('/vendas', authMiddleware, vendaController.cadastrar);
+vendaRouter.post('/vendas', authMiddleware, validator(validadorCadastroVenda) , vendaController.cadastrar);
 
 export default vendaRouter;
