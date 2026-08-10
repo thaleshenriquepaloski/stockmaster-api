ca8f42a76494612c2c09fa1fb91b00239ef8ad99
Thales <devthalespaloski@gmail.com>
2026-08-10 16:54:24 -0300

    feat: implementa contenerizacao com docker e postgresql

--- src/database/prisma.js
+++ src/database/prisma.js
@@ -1,10 +1,12 @@
 import "dotenv/config";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/index.js";
 
-const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL || 'file:./dev.db'
-});
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({ adapter });
 
